9e91a385b0fc58bb0e6db0f9a7c10abda6a09d5c
נֵתָנַאֵל פַּשָׁטִיצֵקִי <50865903+Pashtitzky@users.noreply.github.com>
2020-01-13 00:10:44 +0200

    Update java.js

--- poly/java.js
+++ poly/java.js
@@ -1,15 +1,15 @@
 var starts = [
-"matrix(1, 0, 0, 1, 100, 0)",
-"matrix(1, 0, 0, 1, 90, -10)",
-"matrix(1, 0, 0, 1, 80, -20)",
-"matrix(1, 0, 0, 1, 70, -30)",
-"matrix(1, 0, 0, 1, 60, -40)",
-"matrix(1, 0, 0, 1, 50, -50)",
-"matrix(1, 0, 0, 1, 40, -60)",
-"matrix(1, 0, 0, 1, 30, -70)",
-"matrix(1, 0, 0, 1, 20, -80)",
-"matrix(1, 0, 0, 1, 10, -90)",
-"matrix(1, 0, 0, 1, 0, -100)"
+"matrix(1, 0, 0, 1, 1000, 0)",
+"matrix(1, 0, 0, 1, 900, -100)",
+"matrix(1, 0, 0, 1, 800, -200)",
+"matrix(1, 0, 0, 1, 700, -300)",
+"matrix(1, 0, 0, 1, 600, -400)",
+"matrix(1, 0, 0, 1, 500, -500)",
+"matrix(1, 0, 0, 1, 400, -600)",
+"matrix(1, 0, 0, 1, 300, -700)",
+"matrix(1, 0, 0, 1, 200, -800)",
+"matrix(1, 0, 0, 1, 100, -900)",
+"matrix(1, 0, 0, 1, 0, -1000)"
 ];
 var random_starts = starts[Math.floor(Math.random() * starts.length)];
 document.getElementById("body").style.transform = random_starts;
