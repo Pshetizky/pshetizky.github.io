92bddf1d904064532af348c396827b3f8ccc580c
נֵתָנַאֵל פַּשָׁטִיצֵקִי <50865903+Pashtitzky@users.noreply.github.com>
2020-07-05 00:10:03 +0300

    Create index.js

---
+++ memod/index_files/index.js
@@ -0,0 +1,64 @@
+// array 001 sets001 english
+var sets = [
+'"What makes me happy in my life now?"',
+'"What excites me in my life now?"',
+'"What gives me gratitude in my life now?"',
+'"Who do I love in my life now?"',
+'"Who loves me in my life now?"',
+'"What habit do I think I am obligated to create in my life now?"',
+'"What subconscious beliefs am I obligated to create in my life now?"'
+];
+function getSets() {
+	return sets[
+		Math.floor(Math.random() * sets.length)
+	];
+}
+document.documentElement.style.setProperty('--sets001', getSets());
+// array 002 name002 hebrew
+var sets = [
+'"מה משמח אותי בחיי כעת?"',
+'"מה מרגש אותי בחיי כעת?"',
+'"מה מעורר בי הכרת תודה בחיי כעת?"',
+'"את מי אני אוהב בחיי כעת?"',
+'"מי אוהב אותי בחיי כעת?"',
+'"איזה הרגל מחשבתי אני מחוייב/ת ליצור בחיי כעת?"',
+'"איזו אמונה תת הכרתית אני מחוייב/ת ליצור בחיי כעת?"'
+];
+function getNames() {
+	return sets[
+		Math.floor(Math.random() * sets.length)
+	];
+}
+document.documentElement.style.setProperty('--sets002', getSets());
+// array 003 name003 arabic
+var sets = [
+'"ما الذي يجعلني سعيدا في حياتي الآن؟"',
+'"ما الذي يثيرني في حياتي الآن؟"',
+'"ما الذي يعطيني الامتنان في حياتي الآن؟"',
+'"من أحب في حياتي الآن؟"',
+'"من يحبني في حياتي الآن؟"',
+'"ما هي العادة التي أعتقد أنني ملزمة بخلقها في حياتي الآن؟"',
+'"ما المعتقدات اللاواعية التي ألتزم بخلقها في حياتي الآن؟"'
+];
+function getNames() {
+	return sets[
+		Math.floor(Math.random() * sets.length)
+	];
+}
+document.documentElement.style.setProperty('--sets003', getSets());
+// array 004 name004 russian
+var sets = [
+'"Что делает меня счастливым в моей жизни сейчас?"',
+'"Что меня сейчас волнует в жизни?"',
+'"Что дает мне благодарность в моей жизни сейчас?"',
+'"Кого я люблю в своей жизни сейчас?"',
+'"Кто любит меня в моей жизни сейчас?"',
+'"Какую привычку я думаю, я обязан создать в моей жизни сейчас?"',
+'"Какие подсознательные убеждения я обязан создать в своей жизни сейчас?"'
+];
+function getNames() {
+	return sets[
+		Math.floor(Math.random() * sets.length)
+	];
+}
+document.documentElement.style.setProperty('--sets004', getSets());
