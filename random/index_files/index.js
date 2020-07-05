659d169495b2abb848376059b9ed0e3477353efc
נֵתָנַאֵל פַּשָׁטִיצֵקִי <50865903+Pashtitzky@users.noreply.github.com>
2020-07-05 22:45:37 +0300

    Update index.js

--- random/index_files/index.js
+++ random/index_files/index.js
@@ -45,3 +45,58 @@ document.documentElement.style.setProperty('--let009', getLet());
 document.documentElement.style.setProperty('--let010', getLet());
 document.documentElement.style.setProperty('--let011', getLet());
 document.documentElement.style.setProperty('--let012', getLet());
+// Array 002 big english to numbers
+var engtonum = [
+"'A'",
+"'B'",
+"'C'",
+"'D'",
+"'E'",
+"'F'",
+"'G'",
+"'H'",
+"'I'",
+"'j'",
+"'K'",
+"'L'",
+"'M'",
+"'N'",
+"'O'",
+"'P'",
+"'Q'",
+"'R'",
+"'S'",
+"'T'",
+"'U'",
+"'V'",
+"'W'",
+"'X'",
+"'Y'",
+"'Z'",
+"'0'","'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'","'9'"
+];
+function getLet() {
+	return engtonum[
+		Math.floor(Math.random() * engtonum.length)
+	];
+}
+document.documentElement.style.setProperty('--se001', getLet());
+document.documentElement.style.setProperty('--se002', getLet());
+document.documentElement.style.setProperty('--se003', getLet());
+document.documentElement.style.setProperty('--se004', getLet());
+document.documentElement.style.setProperty('--se005', getLet());
+document.documentElement.style.setProperty('--se006', getLet());
+document.documentElement.style.setProperty('--se007', getLet());
+document.documentElement.style.setProperty('--se008', getLet());
+document.documentElement.style.setProperty('--se009', getLet());
+document.documentElement.style.setProperty('--se010', getLet());
+document.documentElement.style.setProperty('--se011', getLet());
+document.documentElement.style.setProperty('--se012', getLet());
+document.documentElement.style.setProperty('--se013', getLet());
+document.documentElement.style.setProperty('--se014', getLet());
+document.documentElement.style.setProperty('--se015', getLet());
+document.documentElement.style.setProperty('--se016', getLet());
+document.documentElement.style.setProperty('--se017', getLet());
+document.documentElement.style.setProperty('--se018', getLet());
+document.documentElement.style.setProperty('--se019', getLet());
+document.documentElement.style.setProperty('--se020', getLet());
