50fd08059b31f3563b1a5ccbb0370fe56da63ce2
נֵתָנַאֵל פַּשָׁטִיצֵקִי <50865903+Pashtitzky@users.noreply.github.com>
2020-07-06 19:48:08 +0300

    Update index.js

--- random/index_files/index.js
+++ random/index_files/index.js
@@ -1,5 +1,5 @@
 // Array 001 english to numbers
-var engtonum = [
+var b001 = [
 "'a'","'A'",
 "'b'","'B'",
 "'c'","'C'",
@@ -28,25 +28,25 @@ var engtonum = [
 "'z'","'Z'",
 "'0'","'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'","'9'"
 ];
-function getLet() {
-	return engtonum[
-		Math.floor(Math.random() * engtonum.length)
+function getB001() {
+	return b001[
+		Math.floor(Math.random() * b001.length)
 	];
 }
-document.documentElement.style.setProperty('--let001', getLet());
-document.documentElement.style.setProperty('--let002', getLet());
-document.documentElement.style.setProperty('--let003', getLet());
-document.documentElement.style.setProperty('--let004', getLet());
-document.documentElement.style.setProperty('--let005', getLet());
-document.documentElement.style.setProperty('--let006', getLet());
-document.documentElement.style.setProperty('--let007', getLet());
-document.documentElement.style.setProperty('--let008', getLet());
-document.documentElement.style.setProperty('--let009', getLet());
-document.documentElement.style.setProperty('--let010', getLet());
-document.documentElement.style.setProperty('--let011', getLet());
-document.documentElement.style.setProperty('--let012', getLet());
+document.documentElement.style.setProperty('--b001', getB001());
+document.documentElement.style.setProperty('--b002', getB001());
+document.documentElement.style.setProperty('--b003', getB001());
+document.documentElement.style.setProperty('--b004', getB001());
+document.documentElement.style.setProperty('--b005', getB001());
+document.documentElement.style.setProperty('--b006', getB001());
+document.documentElement.style.setProperty('--b007', getB001());
+document.documentElement.style.setProperty('--b008', getB001());
+document.documentElement.style.setProperty('--b009', getB001());
+document.documentElement.style.setProperty('--b010', getB001());
+document.documentElement.style.setProperty('--b011', getB001());
+document.documentElement.style.setProperty('--b012', getB001());
 // Array 002 big english to numbers
-var engtonum = [
+var c001 = [
 "'A'",
 "'B'",
 "'C'",
@@ -75,28 +75,57 @@ var engtonum = [
 "'Z'",
 "'0'","'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'","'9'"
 ];
-function getLet() {
-	return engtonum[
-		Math.floor(Math.random() * engtonum.length)
+function getC001() {
+	return c001[
+		Math.floor(Math.random() * c001.length)
 	];
 }
-document.documentElement.style.setProperty('--se001', getLet());
-document.documentElement.style.setProperty('--se002', getLet());
-document.documentElement.style.setProperty('--se003', getLet());
-document.documentElement.style.setProperty('--se004', getLet());
-document.documentElement.style.setProperty('--se005', getLet());
-document.documentElement.style.setProperty('--se006', getLet());
-document.documentElement.style.setProperty('--se007', getLet());
-document.documentElement.style.setProperty('--se008', getLet());
-document.documentElement.style.setProperty('--se009', getLet());
-document.documentElement.style.setProperty('--se010', getLet());
-document.documentElement.style.setProperty('--se011', getLet());
-document.documentElement.style.setProperty('--se012', getLet());
-document.documentElement.style.setProperty('--se013', getLet());
-document.documentElement.style.setProperty('--se014', getLet());
-document.documentElement.style.setProperty('--se015', getLet());
-document.documentElement.style.setProperty('--se016', getLet());
-document.documentElement.style.setProperty('--se017', getLet());
-document.documentElement.style.setProperty('--se018', getLet());
-document.documentElement.style.setProperty('--se019', getLet());
-document.documentElement.style.setProperty('--se020', getLet());
+document.documentElement.style.setProperty('--c001', getC001());
+document.documentElement.style.setProperty('--c002', getC001());
+document.documentElement.style.setProperty('--c003', getC001());
+document.documentElement.style.setProperty('--c004', getC001());
+document.documentElement.style.setProperty('--c005', getC001());
+document.documentElement.style.setProperty('--c006', getC001());
+document.documentElement.style.setProperty('--c007', getC001());
+document.documentElement.style.setProperty('--c008', getC001());
+document.documentElement.style.setProperty('--c009', getC001());
+document.documentElement.style.setProperty('--c010', getC001());
+document.documentElement.style.setProperty('--c011', getC001());
+document.documentElement.style.setProperty('--c012', getC001());
+document.documentElement.style.setProperty('--c013', getC001());
+document.documentElement.style.setProperty('--c014', getC001());
+document.documentElement.style.setProperty('--c015', getC001());
+document.documentElement.style.setProperty('--c016', getC001());
+document.documentElement.style.setProperty('--c017', getC001());
+document.documentElement.style.setProperty('--c018', getC001());
+document.documentElement.style.setProperty('--c019', getC001());
+document.documentElement.style.setProperty('--c020', getC001());
+// Array 002 big english to numbers
+var d001 = [
+"'0'","'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'","'9'"
+];
+function getD001() {
+	return d001[
+		Math.floor(Math.random() * d001.length)
+	];
+}
+document.documentElement.style.setProperty('--d001', getD001());
+document.documentElement.style.setProperty('--d002', getD001());
+document.documentElement.style.setProperty('--d003', getD001());
+document.documentElement.style.setProperty('--d004', getD001());
+document.documentElement.style.setProperty('--d005', getD001());
+document.documentElement.style.setProperty('--d006', getD001());
+document.documentElement.style.setProperty('--d007', getD001());
+document.documentElement.style.setProperty('--d008', getD001());
+document.documentElement.style.setProperty('--d009', getD001());
+document.documentElement.style.setProperty('--d010', getD001());
+document.documentElement.style.setProperty('--d011', getD001());
+document.documentElement.style.setProperty('--d012', getD001());
+document.documentElement.style.setProperty('--d013', getD001());
+document.documentElement.style.setProperty('--d014', getD001());
+document.documentElement.style.setProperty('--d015', getD001());
+document.documentElement.style.setProperty('--d016', getD001());
+document.documentElement.style.setProperty('--d017', getD001());
+document.documentElement.style.setProperty('--d018', getD001());
+document.documentElement.style.setProperty('--d019', getD001());
+document.documentElement.style.setProperty('--d020', getD001());
