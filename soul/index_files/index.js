0e2647740194c9bc6828acb2791184dd3eb4fd79
נֵתָנַאֵל פַּשָׁטִיצֵקִי <50865903+Pashtitzky@users.noreply.github.com>
2020-07-16 18:56:10 +0300

    Update index.js

--- soul/index_files/index.js
+++ soul/index_files/index.js
@@ -1,18 +1,18 @@
 // array 001 name001 english
 var names = [
-'"named after Yehuda Abarbanel"',
+'"Named after Yehuda Abarbanel"',
 '"Geha"',
 '"Beer Jacob"',
 '"Ness Ziona"',
-'"named after Suraski"'
+'"Named after Suraski"'
 ];
 function getNames() {
 	return names[
 		Math.floor(Math.random() * names.length)
 	];
 }
-document.documentElement.style.setProperty('--name001', getNames());
-// array 002 name002 hebrew
+document.documentElement.style.setProperty('--a001', getNames());
+// array 002 a002 hebrew
 var names = [
 '"על-שם יהודה אברבנאל"',
 '"גהה"',
@@ -25,8 +25,8 @@ function getNames() {
 		Math.floor(Math.random() * names.length)
 	];
 }
-document.documentElement.style.setProperty('--name002', getNames());
-// array 003 name003 arabic
+document.documentElement.style.setProperty('--a002', getNames());
+// array 003 a003 arabic
 var names = [
 '"أباربانيل على اسم"',
 '"جاها"',
@@ -39,8 +39,8 @@ function getNames() {
 		Math.floor(Math.random() * names.length)
 	];
 }
-document.documentElement.style.setProperty('--name003', getNames());
-// array 004 name004 russian
+document.documentElement.style.setProperty('--a003', getNames());
+// array 004 a004 russian
 var names = [
 '"Там Абарбанель"',
 '"Геха"',
@@ -53,4 +53,4 @@ function getNames() {
 		Math.floor(Math.random() * names.length)
 	];
 }
-document.documentElement.style.setProperty('--name004', getNames());
+document.documentElement.style.setProperty('--a004', getNames());
