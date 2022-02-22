b3e09fcbb4701cf9151386fbd3f36018a92cb42a
Netanel Pshetizky <50865903+Pshetizky@users.noreply.github.com>
2022-02-22 15:48:43 +0200

    Hi!

--- random/index_files/index.js
+++ random/index_files/index.js
@@ -1,4 +1,4 @@
-// Array 001 All
+// Array 001 English with numbers
 var b001 = [
 "'a'","'A'",
 "'b'","'B'",
@@ -45,3 +45,27 @@ document.documentElement.style.setProperty('--b009', getB001());
 document.documentElement.style.setProperty('--b010', getB001());
 document.documentElement.style.setProperty('--b011', getB001());
 document.documentElement.style.setProperty('--b012', getB001());
+
+// Array 002 Hebrew
+var b001 = [
+"'א'","'ב'","'ג'","'ד'","'ה'","'ו'","'ז'","'ח'","'ט'","'י'","'כ'","'ל'","'מ'","'נ'","'ס'","'ע'","'פ'","'צ'","'ק'","'ר'","'ש'","'ת'"
+];
+function getB001() {
+	return b001[
+		Math.floor(Math.random() * b001.length)
+	];
+}
+document.documentElement.style.setProperty('--c001', getB001());
+document.documentElement.style.setProperty('--c002', getB001());
+document.documentElement.style.setProperty('--c003', getB001());
+document.documentElement.style.setProperty('--c004', getB001());
+document.documentElement.style.setProperty('--c005', getB001());
+document.documentElement.style.setProperty('--c006', getB001());
+document.documentElement.style.setProperty('--c007', getB001());
+document.documentElement.style.setProperty('--c008', getB001());
+document.documentElement.style.setProperty('--c009', getB001());
+document.documentElement.style.setProperty('--c010', getB001());
+document.documentElement.style.setProperty('--c011', getB001());
+document.documentElement.style.setProperty('--c012', getB001());
+
+
