a9fdb33a9d51d2de4e5ba7c10dffe24890ba0c97
Netanel Pshetizky <50865903+Pshetizky@users.noreply.github.com>
2022-02-22 21:00:53 +0200

    Hello

--- random/index_files/index.js
+++ random/index_files/index.js
@@ -1,4 +1,4 @@
-// Array 001 English with numbers
+// Array 001 Keys
 var b001 = [
 "'a'","'A'",
 "'b'","'B'",
@@ -46,7 +46,76 @@ document.documentElement.style.setProperty('--b010', getB001());
 document.documentElement.style.setProperty('--b011', getB001());
 document.documentElement.style.setProperty('--b012', getB001());
 
-// Array 002 Hebrew
+// Array 002 Numbers
+var b001 = [
+"'0'","'1'","'2'","'3'","'4'","'5'","'6'","'7'","'8'","'9'"
+];
+function getB001() {
+	return b001[
+		Math.floor(Math.random() * b001.length)
+	];
+}
+document.documentElement.style.setProperty('--kaf1', getB001());
+document.documentElement.style.setProperty('--kaf2', getB001());
+document.documentElement.style.setProperty('--kaf3', getB001());
+document.documentElement.style.setProperty('--kaf4', getB001());
+document.documentElement.style.setProperty('--kaf5', getB001());
+document.documentElement.style.setProperty('--kaf6', getB001());
+document.documentElement.style.setProperty('--kaf7', getB001());
+document.documentElement.style.setProperty('--kaf8', getB001());
+document.documentElement.style.setProperty('--kaf9', getB001());
+document.documentElement.style.setProperty('--kaf10', getB001());
+document.documentElement.style.setProperty('--kaf11', getB001());
+document.documentElement.style.setProperty('--kaf12', getB001());
+
+// Array 006 English
+var b001 = [
+"'a'","'A'",
+"'b'","'B'",
+"'c'","'C'",
+"'d'","'D'",
+"'e'","'E'",
+"'f'","'F'",
+"'g'","'G'",
+"'h'","'H'",
+"'i'","'I'",
+"'j'","'j'",
+"'k'","'K'",
+"'l'","'L'",
+"'m'","'M'",
+"'n'","'N'",
+"'o'","'O'",
+"'p'","'P'",
+"'q'","'Q'",
+"'r'","'R'",
+"'s'","'S'",
+"'t'","'T'",
+"'u'","'U'",
+"'v'","'V'",
+"'w'","'W'",
+"'x'","'X'",
+"'y'","'Y'",
+"'z'","'Z'"
+];
+function getB001() {
+	return b001[
+		Math.floor(Math.random() * b001.length)
+	];
+}
+document.documentElement.style.setProperty('--yahel1', getB001());
+document.documentElement.style.setProperty('--yahel2', getB001());
+document.documentElement.style.setProperty('--yahel3', getB001());
+document.documentElement.style.setProperty('--yahel4', getB001());
+document.documentElement.style.setProperty('--yahel5', getB001());
+document.documentElement.style.setProperty('--yahel6', getB001());
+document.documentElement.style.setProperty('--yahel7', getB001());
+document.documentElement.style.setProperty('--yahel8', getB001());
+document.documentElement.style.setProperty('--yahel9', getB001());
+document.documentElement.style.setProperty('--yahel10', getB001());
+document.documentElement.style.setProperty('--yahel11', getB001());
+document.documentElement.style.setProperty('--yahel12', getB001());
+
+// Array 007 Hebrew
 var b001 = [
 "'א'","'ב'","'ג'","'ד'","'ה'","'ו'","'ז'","'ח'","'ט'","'י'","'כ'","'ל'","'מ'","'נ'","'ס'","'ע'","'פ'","'צ'","'ק'","'ר'","'ש'","'ת'"
 ];
@@ -67,5 +136,79 @@ document.documentElement.style.setProperty('--c009', getB001());
 document.documentElement.style.setProperty('--c010', getB001());
 document.documentElement.style.setProperty('--c011', getB001());
 document.documentElement.style.setProperty('--c012', getB001());
+// Array 008 Arabic
+var b001 = [
+"'ا'","'ب'","'ت'","'ج'","'ح'","'خ'","'د'","'ذ'","'ر'","'ز'","'س'","'ش'","'ص'","'ض'",
+"'ط'","'ظ'","'ع'","'غ'","'ف'","'ق'","'ك'","'ل'","'م'","'ن'","'ه'","'و'","'ي'"
+];
+function getB001() {
+	return b001[
+		Math.floor(Math.random() * b001.length)
+	];
+}
+document.documentElement.style.setProperty('--thashat1', getB001());
+document.documentElement.style.setProperty('--thashat2', getB001());
+document.documentElement.style.setProperty('--thashat3', getB001());
+document.documentElement.style.setProperty('--thashat4', getB001());
+document.documentElement.style.setProperty('--thashat5', getB001());
+document.documentElement.style.setProperty('--thashat6', getB001());
+document.documentElement.style.setProperty('--thashat7', getB001());
+document.documentElement.style.setProperty('--thashat8', getB001());
+document.documentElement.style.setProperty('--thashat9', getB001());
+document.documentElement.style.setProperty('--thashat10', getB001());
+document.documentElement.style.setProperty('--thashat11', getB001());
+document.documentElement.style.setProperty('--thashat12', getB001());
+// Array 008 Russian
+var b001 = [
 
-
+"'а'","'А'",
+"'б'","'Б'",
+"'в'","'В'",
+"'г'","'Г'",
+"'д'","'Д'",
+"'е'","'Е'",
+"'а'","'А'",
+"'ё'","'Ё'",
+"'ж'","'Ж'",
+"'з'","'З'",
+"'и'","'И'",
+"'й'","'Й'",
+"'к'","'К'",
+"'л'","'Л'",
+"'м'","'М'",
+"'н'","'Н'",
+"'о'","'О'",
+"'п'","'П'",
+"'р'","'Р'",
+"'с'","'С'",
+"'т'","'Т'",
+"'у'","'У'",
+"'ф'","'Ф'",
+"'х'","'Х'",
+"'ч'","'Ц'",
+"'ш'","'Ш'",
+"'щ'","'Щ'",
+"'ъ'","'Ъ'",
+"'ы'","'Ы'",
+"'ь'","'Ь'",
+"'э'","'Э'",
+"'ю'","'Ю'",
+"'я'","'Я'"
+];
+function getB001() {
+	return b001[
+		Math.floor(Math.random() * b001.length)
+	];
+}
+document.documentElement.style.setProperty('--shaii1', getB001());
+document.documentElement.style.setProperty('--shaii2', getB001());
+document.documentElement.style.setProperty('--shaii3', getB001());
+document.documentElement.style.setProperty('--shaii4', getB001());
+document.documentElement.style.setProperty('--shaii5', getB001());
+document.documentElement.style.setProperty('--shaii6', getB001());
+document.documentElement.style.setProperty('--shaii7', getB001());
+document.documentElement.style.setProperty('--shaii8', getB001());
+document.documentElement.style.setProperty('--shaii9', getB001());
+document.documentElement.style.setProperty('--shaii10', getB001());
+document.documentElement.style.setProperty('--shaii11', getB001());
+document.documentElement.style.setProperty('--shaii12', getB001());
