2f3b12234dcb0c6af961759b7ded4d5a8b6e9191
Netanel Pshetizky <50865903+Pshetizky@users.noreply.github.com>
2022-08-02 22:18:26 +0300

    Create do.js

---
+++ random/do.js
@@ -0,0 +1,7 @@
+node --random_seed= / node
+Array.from(Array(5), Math.random)
+sequence = []
+python3 main.py
+node
+.toString(36).slice(-11).split('').map(c => Math.random() < 0.5 ? c.toUpperCase() : c).join('')
+
