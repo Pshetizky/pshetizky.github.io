51b4fa5de14649929f5d9b8e71dd106a73e99e17
Netanel Pshetizky <50865903+Pshetizky@users.noreply.github.com>
2024-12-12 02:00:45 +0200

    December

--- a/main.py
+++ a/main.py
@@ -1,7 +1,7 @@
-import string
-import secrets
-length = int(input())
-if length <= 4: length = 5
+import string, secrets
+from pyscript import display
+##length = int(input())
+length = int(20)
 alphabet = string.ascii_letters + string.digits + "~!@#$%^&*_+-=.,:"
 while True:
    password = ''.join(secrets.choice(alphabet) for i in range(length))
@@ -9,19 +9,19 @@ while True:
          and any(c.isupper() for c in password)
          and sum(c.isdigit() for c in password) >= 3):
       break
-print("PASSWORD: ",password)
-english = string.ascii_letters
-preview_eng = ''.join(secrets.choice(english) for i in range(length))
-print("ENGLISH NAME: ",preview_eng)
-russian = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
-preview_rus = ''.join(secrets.choice(russian) for i in range(length))
-print("RUSSIAN NAME: ",preview_rus)
-arabic = "ابتثجحخدذرزسشصضطظعغفقكلمنهوي"
-preview_arb = ''.join(secrets.choice(arabic) for i in range(length))
-print("ARABIC NAME: ",preview_arb)
-hebrew = "אבגדהוזחטיכלמנסעפצקרשת"
-preview_heb = ''.join(secrets.choice(hebrew) for i in range(length))
-print("HEBREW NAME: ",preview_heb)
+display(password)
 numbers = string.digits
 preview_num = ''.join(secrets.choice(numbers) for i in range(length))
-print("NUMBERS: ",preview_num," ")
+display(preview_num)
+english = string.ascii_letters
+preview_eng = ''.join(secrets.choice(english) for i in range(length))
+display(preview_eng)
+russian = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя"
+preview_rus = ''.join(secrets.choice(russian) for i in range(length))
+display(preview_rus)
+arabic = "ابتثجحخدذرزسشصضطظعغفقكلمنهوي"
+preview_arb = ''.join(secrets.choice(arabic) for i in range(length))
+display(preview_arb)
+hebrew = "אבגדהוזחטיכלמנסעפצקרשת"
+preview_heb = ''.join(secrets.choice(hebrew) for i in range(length))
+display(preview_heb)
